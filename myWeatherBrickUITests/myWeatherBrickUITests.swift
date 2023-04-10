4eb180ddc081b8a841a7980aa655ad69fa5f84a7
yuraSudec <sabadin85@icloud.com>
2023-04-10 14:12:57 +0300

    write ui test

--- myWeatherBrickUITests/myWeatherBrickUITests.swift
+++ myWeatherBrickUITests/myWeatherBrickUITests.swift
@@ -1,41 +1,43 @@
-//
-//  myWeatherBrickUITests.swift
-//  myWeatherBrickUITests
-//
-//  Created by YURA																			 on 10.03.2023.
-//
 
 import XCTest
+@testable import myWeatherBrick
 
 final class myWeatherBrickUITests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    
+    func testAppWeather() {
         let app = XCUIApplication()
         app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let scrollViewsQuery = app.scrollViews
+        let infoElement = scrollViewsQuery.otherElements.containing(.button, identifier:"INFO").element
+        
+        
+        let elementsQuery = scrollViewsQuery.otherElements
+        XCTAssert(app.staticTexts["20.0"].exists)
+        
+        
+        let iconLocationButton = elementsQuery.buttons["icon location"]
+        iconLocationButton.tap()
+        
+        elementsQuery.buttons["icon search"].tap()
+        app.alerts["Find City"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Input City name:"]/*[[".cells.textFields[\"Input City name:\"]",".textFields[\"Input City name:\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.alerts["Find City"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["Input City name:"]/*[[".cells.textFields[\"Input City name:\"]",".textFields[\"Input City name:\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.typeText("Paris")
+        app.alerts["Find City"].scrollViews.otherElements.buttons["Find"].tap()
+       
+        XCTAssert(app.staticTexts["14.0"].exists)
+       
+        elementsQuery.buttons["INFO"].tap()
+        
+        XCTAssert(app.staticTexts["Hide"].exists)
+        app.staticTexts["Hide"].tap()
+       
+        infoElement.tap()
+        infoElement.swipeDown()
+       
+        XCTAssert(app.staticTexts["20.0"].exists)
+        
+   
+        
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
